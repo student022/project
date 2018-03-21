c404cd90031f9c87d6e63aaa622bf79224db7d85
student022 <37609296+student022@users.noreply.github.com>
2018-03-21 16:20:43 +0530

    cleared error messages
    no error messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,21 +11,7 @@
 
 @section('content')
     
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-              @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-            @endif
+        
 
             <div class="row">
             <!--row starts-->
